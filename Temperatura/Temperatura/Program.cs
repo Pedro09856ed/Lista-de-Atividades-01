a1c0aeacd68d875d6708be4d4fe2b18a3d0e6da6
Pedro Calmon <phcalmonfoletto@gmail.com>
2025-09-24 09:32:01 -0300

    Autoria

---
+++ Temperatura/Temperatura/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escolha a conversão:");
+        Console.WriteLine("1 - Celsius para Fahrenheit");
+        Console.WriteLine("2 - Fahrenheit para Celsius");
+        Console.Write("Opção: ");
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+        if (opcao == 1)
+        {
+            Console.Write("Digite a temperatura em Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine("Temperatura em Fahrenheit: " + fahrenheit + "°F");
+        }
+        else if (opcao == 2)
+        {
+            Console.Write("Digite a temperatura em Fahrenheit: ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Temperatura em Celsius: " + celsius + "°C");
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+        }
+    }
+}
